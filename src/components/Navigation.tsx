24ad55d5b5cb47806b64bdf2c6c40e92ee06b0e1
chest4 <ches.star@mail.ru>
2023-10-18 16:05:43 +0400

    container

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,8 +8,8 @@ export default function Navigation() {
 	const t = useTranslations('Navigation');
 
 	return (
-		<div className="bg-slate-850 flex items-center">
-			<nav className="container mx-auto flex justify-between text-white py-3">
+		<div className="flex bg-black items-center">
+			<nav className="container max-w-3xl mx-auto flex justify-between text-white py-3">
 				<ul className='flex gap-5'>
 					<li className='flex items-center'><NavigationLink href="/">{t('home')}</NavigationLink></li>
 					<li className='flex items-center'><NavigationLink href="/about">{t('about')}</NavigationLink></li>
